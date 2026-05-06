19c487090435d08ed4c6da418f0783e8e5ec5d78
apexlnc <43242113+apexlnc@users.noreply.github.com>
2026-05-06 09:15:11 -0400

    refactor(oidc): rename result collection constructor

--- controller/pkg/agentgateway/oidc/cache.go
+++ controller/pkg/agentgateway/oidc/cache.go
@@ -5,9 +5,9 @@ import (
 )
 
 // OidcResults stores discovered OIDC providers as a KRT-visible collection.
-type OidcResults = remotecache.Results[DiscoveredProvider]
+type OidcResults = remotecache.FetchedResults[DiscoveredProvider]
 
-// NewResults constructs an empty OIDC result collection.
-func NewResults() *OidcResults {
-	return remotecache.NewResults[DiscoveredProvider]()
+// NewFetchedResults constructs an empty OIDC fetched-result collection.
+func NewFetchedResults() *OidcResults {
+	return remotecache.NewFetchedResults[DiscoveredProvider]()
 }
